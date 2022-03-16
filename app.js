b76c4273ca61a6d2f81b57f5c78d924fc4587d48
pixls <tha.pixls@gmail.com>
2022-03-16 18:16:54 -0400

    works!

---
+++ app.js
@@ -0,0 +1,39 @@
+const keybWords = [
+    'keyb',
+    'kb',
+    'alpha',
+    'switch',
+    'clack',
+    'thock',
+    'keeb',
+    'key',
+    'heavy',
+    'lab',
+    'labs',
+    'lube',
+    'kbd',
+    'keys',
+    'type',
+    'typist',
+    'thocc'
+]
+
+function pickWord() {
+    let pick = Math.floor(Math.random() * keybWords.length)
+    return keybWords[pick]
+}
+
+function getName() {
+    let word1 = pickWord();
+    let word2 = pickWord();
+    if (word1 == word2) {
+        word2 = pickWord();
+    }
+    return `${word1}${word2}`
+}
+
+$('#generate').click(function(evt){
+    evt.preventDefault();
+    const newName = getName();
+    $('#nameField').text(newName).class("display-3 text-body")
+})
